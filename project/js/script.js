7eeb79d5689bb49938fe44ef167b39317c7fdd54
Shama8nchez <shamanouski@gmail.com>
2022-05-05 16:42:10 +0300

    feat: add h1

---
+++ project/js/script.js
@@ -0,0 +1,7 @@
+const body = document.body;
+const title = document.createElement('h1');
+title.classList.add('title');
+title.innerHTML = 'Virtual Keyboard';
+console.log(title);
+document.body.appendChild(title);
+
