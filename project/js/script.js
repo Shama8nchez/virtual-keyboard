16feaaaf19b3486343b01f83d82cca6d47d9dc47
Shama8nchez <shamanouski@gmail.com>
2022-05-05 16:46:43 +0300

    feat: add textarea

--- project/js/script.js
+++ project/js/script.js
@@ -1,7 +1,13 @@
 const body = document.body;
+
 const title = document.createElement('h1');
 title.classList.add('title');
 title.innerHTML = 'Virtual Keyboard';
-console.log(title);
 document.body.appendChild(title);
 
+const field = document.createElement('textarea');
+field.name = 'field';
+field.id = 'text-field';
+field.cols = '101';
+field.rows = '10';
+document.body.appendChild(field);
