842ae2505ba70429409f74afd1ec4412b9e8f951
Shama8nchez <shamanouski@gmail.com>
2022-05-06 11:14:33 +0300

    feat: add keydown/keyup

--- project/js/script.js
+++ project/js/script.js
@@ -16,12 +16,13 @@ const keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
 document.body.appendChild(keyboard);
 
-//let arr = [];
+/* let arr = []; */
 let letters = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Up', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Left', 'Down', 'Right', 'Ctrl'];
 let lettersCaps = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Delete', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Up', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Left', 'Down', 'Right', 'Ctrl']
+let keyNum = [1920, 490, 500, 510, 520, 530, 540, 550, 560, 570, 480, 1890, 1870, 80, 90, 810, 870, 690, 820, 840, 890, 850, 730, 790, 800, 2190, 2210, 2200, 460, 200, 650, 830, 680, 700, 710, 720, 740, 750, 760, 1860, 2220, 130, 161, 900, 880, 670, 860, 660, 780, 770, 1880, 1900, 1910, 380, 162, 171, 911, 181, 320, 182, 370, 400, 390, 172]
 
 /* document.onkeydown = function(e) {
-  arr.push(e.key);
+  arr.push(e.keyCode);
   console.log(arr);
 } */
 
@@ -34,6 +35,20 @@ for (let i = 0; i < letters.length; i++) {
   if (letters[i] == ' ') {
     letter.classList.add('keyspace');
   }
-  letter.innerHTML = letters[i]
-  keyboard.appendChild(letter)
+  letter.innerHTML = letters[i];
+  letter.dataset.num = `${keyNum[i]}`
+  keyboard.appendChild(letter);
 }
+
+document.onkeydown = function(e) {
+  const actKey = document.querySelector('.key[data-num="'+e.keyCode+e.location.toString()+'"');
+  actKey.classList.add('key-active')
+}
+
+document.onkeyup = function(e) {
+  const actKey = document.querySelector('.key[data-num="'+e.keyCode+e.location.toString()+'"');
+  actKey.classList.remove('key-active')
+  console.log("Location of key pressed: " + e.location);
+}
+
+
